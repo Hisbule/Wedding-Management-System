bd499c5a0b6ac8c7e4e5bff9626f0965806e7f53
Hafiz Uddin Ahmed Adnan <106032099+Hisbule@users.noreply.github.com>
2024-08-13 22:55:10 +0600

    Add files via upload

---
+++ include/nav.php
@@ -0,0 +1,38 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light" style=display:flex;>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><img src="photo/logo.png" width="50" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php"  >Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="gallery.php">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pricing.php">Packages</a>
+                </li>
+            
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact us</a>
+                </li>
+                
+            </ul>
+            <div class="form-inline mr-2">
+                <!-- <a class="mr-2" href="submit">
+                    <i class="mdi mdi-star-outline" id="review" aria-hidden="true"></i> Write a review</a> -->
+<!--                <a class="btn btn-sm my-2 my-sm-0" href="login.php" data-toggle="modal" data-target="#loginModal">-->
+                <a class="btn btn-sm my-2 my-sm-0" href="admin/login.php">
+                    Admin Login
+                </a>
+                <a class="btn btn-sm my-2 my-sm-0 mr-2 loginbtn" href="sign_up.php">Sign up</a>
+            </div>
+        </div>
+    </div>
+</nav>
